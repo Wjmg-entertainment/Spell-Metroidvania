1e57dc7b8001185868580388eb8f1fb5e0e133ee
EnderTheOne <71531578+EnderTheOne@users.noreply.github.com>
2023-05-25 16:01:03 -0400

    Finished animation transitions
    Added animation transitions for jumping and falling down using the velocity of the players rigid-body. Changed the name of "horizontal" variable to "horizontalMovement" to better represent the variable.

--- Spell Metroidvania/Assets/Scripts/PlayerMovementScript.cs
+++ Spell Metroidvania/Assets/Scripts/PlayerMovementScript.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Class that deals with player movement and communicating to animator which animations to play when.
+ */
 public class PlayerMovementScript : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 10f;
-    private float jumpingPower = 14f;
+    //field declarations
+    private float horizontalMovement;
+    private float verticalMovement;
+    private float speedValue = 10f;
+    private float jumpingValue = 14f;
     private bool isFacingRight = true;
-    //bool jump = false;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -25,17 +29,14 @@ public class PlayerMovementScript : MonoBehaviour
 
     private void handleMovementAndJumping()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-
-        animator.SetFloat("Speed", Mathf.Abs(horizontal));
-
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = rb.velocity.y;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+        animator.SetFloat("Jump", verticalMovement);
 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
-            //jump = true;
-            animator.SetBool("isJumping", true);
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            rb.velocity = new Vector2(rb.velocity.x, jumpingValue);
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
@@ -48,7 +49,7 @@ public class PlayerMovementScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontalMovement * speedValue, rb.velocity.y);
     }
 
 
@@ -57,10 +58,9 @@ public class PlayerMovementScript : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-
     private void flipSprite()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if (isFacingRight && horizontalMovement < 0f || !isFacingRight && horizontalMovement > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
